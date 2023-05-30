66bb644f46aa80b329ecbf3c682c5d2c2f0c6fee
Gokhan Guzel <126562774+gokhanguzel0640@users.noreply.github.com>
2023-05-30 14:06:54 +0300

    30.05/gokhan/v1

--- src/test/java/tests/us_033_034_035/Us033.java
+++ src/test/java/tests/us_033_034_035/Us033.java
@@ -1,6 +1,15 @@
 package tests.us_033_034_035;
 
+import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class Us033 {
+
+    @Test
+    public void test(){
+
+    }
 
 
 }
