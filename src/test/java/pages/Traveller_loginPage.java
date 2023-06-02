6edd7cc291fc581f82711ab9cb7fb56aecd0c882
KubraTankisi <kubratankisi@gmail.com>
2023-06-02 23:08:04 +0300

    kübra 02.06/v1

--- src/test/java/pages/Traveller_loginPage.java
+++ src/test/java/pages/Traveller_loginPage.java
@@ -1,4 +1,33 @@
 package pages;
 
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class Traveller_loginPage {
+    public Traveller_loginPage(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(xpath = "//*[text()='ACCEPT']")
+    public  WebElement acceptButonu;
+
+    @FindBy(xpath="//*[text()='Login']")
+    public WebElement loginButonuGiris;
+
+    @FindBy(xpath = "//input[@name='traveller_email']")
+    public WebElement LoginemailKutusu;
+
+    @FindBy(xpath = "//input[@type='password']")
+    public  WebElement LoginpasswordKutusu;
+
+    @FindBy(xpath="(//*[text()='Login'])[3]")
+    public WebElement loginButonu;
+
+    @FindBy(className = "logo")
+    public WebElement anasayfaGoruntuleme;
+
 }
+
