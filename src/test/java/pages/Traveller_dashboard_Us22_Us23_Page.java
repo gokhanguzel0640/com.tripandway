f617c235680200df99ab38de0e4bac5ea081d6c6
IlknurDogan <ilknur.iralgil@gmail.com>
2023-06-04 01:58:09 +0300

    04.06.2023

--- src/test/java/pages/Traveller_dashboard_Us22_Us23_Page.java
+++ src/test/java/pages/Traveller_dashboard_Us22_Us23_Page.java
@@ -1,4 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+
 public class Traveller_dashboard_Us22_Us23_Page {
+
 }
--- src/test/java/pages/Traveller_loginPage.java
+++ src/test/java/pages/Traveller_loginPage.java
@@ -1,4 +1,32 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class Traveller_loginPage {
+
+
+    public Traveller_loginPage(){
+        PageFactory.initElements(Driver.getDriver(),this);}
+
+
+    @FindBy(xpath ="//*[@class='fas fa-lock']")
+    public WebElement loginlinki;
+
+    @FindBy(xpath ="//*[@name='traveller_email']")
+    public WebElement email;
+
+    @FindBy(xpath = "//*[@name='traveller_password']")
+    public WebElement password;
+
+    @FindBy(xpath = "//*[@name='form1']")
+    public WebElement loginbuton;
+
+    @FindBy(xpath = "//*[@type='button']")
+    public WebElement acceptbuton;
+
+    @FindBy (xpath = "//*[@class='top-header-right']")
+    public WebElement DashboardPanel; //basarili login girisi yapilirsa Dashboard paneli gorunmeli
 }
