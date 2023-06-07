3e61f904ac778436e09a0b947e644aabed5e4d23
Gokhan Guzel <126562774+gokhanguzel0640@users.noreply.github.com>
2023-06-07 23:51:06 +0300

    07.06/Gokhan

--- src/test/java/pages/Admin_dashboard_Us33_Us34_Us35_Page.java
+++ src/test/java/pages/Admin_dashboard_Us33_Us34_Us35_Page.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
+import javax.xml.xpath.XPath;
+
 public class Admin_dashboard_Us33_Us34_Us35_Page {
        public Admin_dashboard_Us33_Us34_Us35_Page (){
                 PageFactory.initElements(Driver.getDriver(),this);
@@ -12,7 +14,7 @@ public class Admin_dashboard_Us33_Us34_Us35_Page {
 
         @FindBy(xpath = "(//a[@class='btn btn-success btn-sm btn-block'])[1]")
         public  WebElement  firstdetailButton;
-        @FindBy(xpath = "(//a[@class='btn btn-success btn-sm btn-block'])[1]")
+        @FindBy(xpath = "(//a[@class='btn btn-success btn-sm btn-block'])[2]")
         public  WebElement  secondDetailButton;
 
         @FindBy(xpath = "//h6[@class='m-0 mt-2 font-weight-bold text-primary']")
@@ -26,6 +28,14 @@ public class Admin_dashboard_Us33_Us34_Us35_Page {
 
         @FindBy(xpath = "//h1[@class='h3 mb-3 text-gray-800 invoice-heading']")
         public WebElement orderInvoice;
+
+        @FindBy(xpath = "(//a[@class='collapse-item'])[12]")
+        public WebElement allSubscribersButton;
+
+        @FindBy(xpath = "//h6[@class='m-0 mt-2 font-weight-bold text-primary']")
+        public WebElement viewSubscribers;
+
+
 
 
 }
