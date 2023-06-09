a63f5bbe6b31a4c75666a9c2706874ef84c255d8
muleykecevik <muleyke.cevik@gmail.com>
2023-06-09 20:15:03 +0300

    09/06/23/muleyke

--- src/test/java/tests/us001_002_003_004/US001.java
+++ src/test/java/tests/us001_002_003_004/US001.java
@@ -3,14 +3,21 @@ package tests.us001_002_003_004;
 import org.testng.annotations.Test;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.ReusableMethods;
+import utilities.TestBaseRapor;
 
-public class US001 {
+public class US001 extends TestBaseRapor {
     //Bir ziyaretçi olarak siteyi kullanabilmek icin
     // web sitesine erişebildiğimi doğrulayabilmeliyim
 
     @Test
-    public void erisimTesti() {
+    public void accessTest() {
+        extentTest=extentReports.createTest("The website is accessible test", " User should be able to verify that I can access the website");
+
         Driver.getDriver().get(ConfigReader.getProperty ("twUrl"));
+        ReusableMethods.bekle(2);
+        extentTest.pass("User can access the website");
+
 
     }
 }
